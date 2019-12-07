3810379b7bbb4c2ee188c61786eaee2b75896c19
youya-dev13 <youya.dev13@gmail.com>
2019-12-07 16:26:44 +0900

    Initial project

---
+++ iTunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by 中山 雄也 on 2019/12/07.
+//  Copyright © 2019 youya.dev13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
